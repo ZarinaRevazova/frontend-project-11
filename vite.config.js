5e497a0d5fa06d2830fa6c58ec9abead5e24621f
Zarina_Mac <zarinarevazova8@gmail.com>
2026-02-13 19:17:42 +0300

    step 7_interim_solved problem with npm run preview rendering and connection between html, css and JS

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,18 @@ import { defineConfig } from 'vite';
 import path from 'node:path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'dist'),
+  // base: '/frontend-project-11/',
+  // root: path.resolve(__dirname),
   server: {
     port: 8080,
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      // input: ['src/js/index.js'],
-      external: ['uniqid'],
-    },
+    // rollupOptions: {
+    // input: 'src/main.js',
+    // external: ['uniqid'],
+    // },
   },
   resolve: {
     alias: {
